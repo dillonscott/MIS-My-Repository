5535a7e0593243cee82c4d84716a208018cbb165
Dillon Scott <dillonscott@ou.edu>
2021-02-01 14:04:14 -0600

    Class work example 1

---
+++ In class example - Feb1/VariablesInputConversion Example/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VariablesInputConversion_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heresMyInt;
+            double heresMyDouble;
+
+            Console.WriteLine($"Please enter a double >>");
+            string answer = Console.ReadLine();
+            heresMyDouble = Convert.ToDouble(answer);
+
+            Console.WriteLine($"Please enter an integer >>");
+            answer = Console.ReadLine();
+            heresMyInt = Convert.ToInt32(answer);
+
+           
+
+            double result = heresMyDouble + heresMyInt;
+
+            Console.WriteLine("The result of " + heresMyDouble.ToString("N3") + " + " +
+                heresMyInt.ToString() + " = " + result.ToString("C5"));
+
+
+        }
+    }
+}
