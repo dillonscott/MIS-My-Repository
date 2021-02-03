643ae6ea3eae238d7271eb2f140453ecd68c8d10
Dillon Scott <dillonscott@ou.edu>
2021-02-03 13:42:21 -0600

    Participation 1 - February 3

---
+++ Participation 3/SumOf3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SumOf3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            const double MAGIC_NUMBER = 7.777; //Local variable that is a constant 
+
+            double heresMyDouble;
+            double heresMyDoubleTwo;
+            double heresMyDoubleThree;
+
+            Console.WriteLine($"Please enter a number >>");
+            string answer = Console.ReadLine();
+            heresMyDouble = Convert.ToDouble(answer);
+
+            Console.WriteLine($"Please enter a second number >>");
+            answer = Console.ReadLine();
+            heresMyDoubleTwo = Convert.ToDouble(answer);
+
+            Console.WriteLine($"Please enter a third double >>");
+            answer = Console.ReadLine();
+            heresMyDoubleThree = Convert.ToDouble(answer);
+
+            double result = heresMyDouble + heresMyDoubleTwo + heresMyDoubleThree;
+
+            Console.WriteLine("The sum of your numbers is " + result.ToString("N3"));
+
+            double product = result * MAGIC_NUMBER;
+
+            Console.WriteLine(result.ToString("N3") + " times 7.777 = " + product);
+
+
+        }
+    }
+}
