0c95c290d45562499a90ed5d37d18ffdb9366332
Dillon Scott <dillonscott@ou.edu>
2021-02-08 13:43:50 -0600

    Create solution for random number part

---
+++ Participation in class random/Participation in class random/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Participation_in_class_random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1,3);
+            Console.WriteLine($"{randomNbr}");
+        }
+    }
+}
