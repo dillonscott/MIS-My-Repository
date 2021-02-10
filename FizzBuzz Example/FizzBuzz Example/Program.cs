e8c5c572bdcebf8f135b7c2ec1fa15b1fa9ea1a4
Dillon Scott <dillonscott@ou.edu>
2021-02-10 13:01:29 -0600

    fizzbuzz example

---
+++ FizzBuzz Example/FizzBuzz Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DEVELOPER_INFO = "Dillon Scott";
+            Random rand = new Random();
+            int randomnumberbetween1and100 = rand.Next(1, 101);
+            
+
+
+            if (randomnumberbetween1and100 % 3 == 0
+                && randomnumberbetween1and100 % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (randomnumberbetween1and100 % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randomnumberbetween1and100 % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(randomnumberbetween1and100);
+
+            }
+
+            Console.WriteLine("Developed by {DEVELOPER_INFO}!");
+        }
+    }
+}
