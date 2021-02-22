ca287fe457b41ba44d6853a9268f928195f4ef06
Dillon Scott <dillonscott@ou.edu>
2021-02-22 13:30:39 -0600

    Loop projects

---
+++ IN class 2-22- cointoss and loops/Loop example in class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Loop_example_in_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string usersanswer = " ";
+
+            //do
+            //{
+            //Console.WriteLine("Knock knock. Who's There? >>");
+            //usersanswer = Console.ReadLine();
+            //} 
+            //while (usersanswer.ToLower() != "banana");
+
+            //Console.WriteLine("Knock knock. Who's There? >>");
+            //usersanswer = Console.ReadLine();
+
+
+            // while (usersanswer.ToLower() != "banana") 
+
+            // {
+            //Console.WriteLine(" knock knock. Who's there? >>");
+            // usersanswer = Console.ReadLine();
+            //}
+
+
+            for (int i = 0; i <= 1000; i++) 
+            {
+                //if (i % 3 == 0) ;
+                //{
+                //Console.WriteLine($"{i} is a multiple of three!");
+                //}
+                Console.WriteLine($"{i} is a multiple of three!");
+            }
+        }
+    }
+}
