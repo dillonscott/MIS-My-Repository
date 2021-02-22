e5172795027f10cf4535addb11475855614d4c92
Dillon Scott <dillonscott@ou.edu>
2021-02-22 13:37:01 -0600

    Loop with method

---
+++ IN class 2-22- cointoss and loops/Loops with methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loops_with_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+        }
+        private static void WhileExample()
+        {
+            int favoriteNumber;
+            Console.WriteLine("What's your favorite number? >>");
+            string answer = Console.ReadLine();
+
+            while (int.TryParse(answer, out favoriteNumber) == false)
+            {
+                Console.WriteLine("INcorrect value. you must enter an integer");
+                answer = Console.ReadLine();
+
+            }
+        }
+    }
+}
