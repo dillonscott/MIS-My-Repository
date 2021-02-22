152618b883102fa75cc96f3d7f66c05c1dfe76f4
Dillon Scott <dillonscott@ou.edu>
2021-02-22 13:11:58 -0600

    Coin toss

---
+++ IN class 2-22- cointoss and loops/IN class 2-22- cointoss and loops/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IN_class_2_22__cointoss_and_loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Random rand = new Random();
+            int randnum = rand.Next(1, 3);
+            Console.WriteLine($"{randnum}");
+
+            const int heads = 1;
+            const int tails = 2;
+
+            Console.WriteLine("Heads or tails");
+            string answer = Console.ReadLine();
+
+            int answerAsInt;
+
+            if (answer.ToLower() == "heads") 
+            {
+                answerAsInt = heads;
+            } 
+            else
+            {
+                answerAsInt = tails;
+            }
+
+            if (answerAsInt == randnum)
+            {
+                Console.WriteLine($"It was {answerAsInt}, You guessed right!");
+            }
+            else
+            {
+                Console.WriteLine($"It was {answerAsInt}, Shucks better luck next time");
+
+            }
+        }
+    }
+}
