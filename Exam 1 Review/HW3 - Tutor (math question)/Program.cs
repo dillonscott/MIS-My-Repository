8b088a4589a755c31c66b0ec4fcbe8d50bbcfac5
Dillon Scott <dillonscott@ou.edu>
2021-03-08 17:35:31 -0600

    Hw 3 - tutor (math question loop)

---
+++ Exam 1 Review/HW3 - Tutor (math question)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HW3___Tutor__math_question_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int left = rand.Next(1, 51);
+            int right = rand.Next(1, 51);
+            int sum = left + right;
+            int ans;
+
+            do
+            {
+                Console.WriteLine($"\t{left} + {right} = ");
+                ans = Convert.ToInt32(Console.ReadLine());
+
+                if (ans != sum)
+                {
+                    Console.WriteLine("Sorry please try again:");
+                }
+                
+
+            } while (ans != sum);
+
+            if (ans == sum)
+            {
+                Console.WriteLine("Congrats, you are correct!");
+            }
+            
+        }
+    }
+}
