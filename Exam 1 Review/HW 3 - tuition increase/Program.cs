06321c7f1171eb1848509e25465812be7e682037
Dillon Scott <dillonscott@ou.edu>
2021-03-08 17:42:34 -0600

    HW 3 - tuition increase

---
+++ Exam 1 Review/HW 3 - tuition increase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_3___tuition_increase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double tuition = 12000;
+            const double tuitionincrease = .05;
+
+            for (int i = 1; i <= 7 ; i++)
+            {
+                tuition = tuition * (1 + tuitionincrease);
+                Console.WriteLine($"\tThe tuition after year {i} will be {tuition.ToString("C2")}");
+            }
+            
+        }
+    }
+}
