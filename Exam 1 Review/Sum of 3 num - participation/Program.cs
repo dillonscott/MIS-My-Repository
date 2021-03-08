07780a27b236ca0adb824966c58574e75880d53f
Dillon Scott <dillonscott@ou.edu>
2021-03-08 14:13:04 -0600

    redid participation 1

---
+++ Exam 1 Review/Sum of 3 num - participation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Sum_of_3_num___participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first number >>");
+            string number1 = Console.ReadLine();
+            double num1 = Convert.ToDouble(number1);
+
+
+            Console.WriteLine("Please enter your second number >>");
+            string number2 = Console.ReadLine();
+            double num2 = Convert.ToDouble(number2);
+
+
+            Console.WriteLine("Please enter your third number >>");
+            string number3 = Console.ReadLine();
+            double num3 = Convert.ToDouble(number3);
+
+            const double multiplier = 7.777;
+            double sum = num1 + num2 + num3;
+            double product = sum * multiplier;
+
+            Console.WriteLine($"{num1} + {num2} + {num3} = {sum.ToString("N3")}");
+            Console.WriteLine($"{sum.ToString("N3")} X {multiplier} = {product.ToString("N3")}");
+
+
+        }
+    }
+}
---
+++ InClass 3-1 StringManipulation/inclass 3-3 string another way/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace inclass_3_3_string_another_way
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string message = "tAylOr SwIFt, tHe WeEknD, aRIANa gRAnde, biLlie eiLiSh";
+            string message2 = "tAylOr SwIFt";
+            int indexofspace = message2.IndexOf(' ');
+           
+
+            
+
+            
+
+                                        
+            string newMessage = message.ToUpper()[0] + message.ToLower().Substring(1);
+            Console.WriteLine($"{newMessage}");
+
+            // square brackets by string is a collection: meaning an array of strings
+            string[] artists = message.Split(',');
+
+            for (int i = 0; i < artists.Length; i++)
+            {
+                string artist = artists[i].Trim();
+                message2 = message2.ToUpper()[0] + message2.ToLower().Substring(1, indexofspace)
+                           // substring starts with a in taylor and ends with the space
+                           + message2.ToUpper()[indexofspace + 1] + message2.ToLower().Substring(indexofspace + 2);
+                //substring starts with S and goes to end
+                Console.WriteLine(message2);
+
+                string[] names = artist.Split(' ');
+                string firstname = names[0];
+                string lastName = names[1];
+
+                string newArtist = firstname.ToUpper()[0] + firstname.ToLower().Substring(1) + " " +
+                                   lastName.ToUpper()[0] + lastName.ToLower().Substring(1);
+                Console.WriteLine(newArtist);
+
+            }
+        }
+    }
+}
