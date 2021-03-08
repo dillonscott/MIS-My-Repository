4283a31ac80a59e929a827dc1a57e2ddbb524966
Dillon Scott <dillonscott@ou.edu>
2021-03-08 15:00:09 -0600

    coin toss participation

---
+++ Exam 1 Review/Coin toss - participation/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Coin_toss___participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heads or Tails?");
+            string ans = Console.ReadLine().ToLower();
+
+            Random rand = new Random();
+            int num = rand.Next(1, 3);
+            string coin;
+
+            if (num == 1)
+            {
+                coin = "heads";
+            }
+            else
+            {
+                coin = "tails";
+            }
+            if (ans == coin)
+            {
+                Console.WriteLine($"You're right, it's {coin}");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry it was {coin}");
+            }
+
+
+        }
+    }
+}
