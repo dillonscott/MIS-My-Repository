238079e2afb553d378e7134f1e398e6922fca309
Dillon Scott <dillonscott@ou.edu>
2021-03-10 10:22:19 -0600

    vowels and consonants again

---
+++ midterm review day of/midterm review day of/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace midterm_review_day_of
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence>>");
+            string sentence = Console.ReadLine().ToLower();
+
+            int vowels = 0;
+            int consonants = 0;
+
+            
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char letter = sentence[i];
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter=='o' || letter=='u')
+                {
+                    vowels++;
+                }
+                else
+                {
+                    consonants++;
+                }
+            }
+           
+
+            
+            Console.WriteLine($"\tYour sentence has {vowels} vowels.");
+            Console.WriteLine($"\tYour sentence has {consonants} consonants.");
+
+        }
+    }
+}
