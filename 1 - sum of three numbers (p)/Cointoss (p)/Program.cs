f5b25ca478832e92e658d3b36a27bce1ea78bb35
Dillon Scott <dillonscott@ou.edu>
2021-03-09 19:25:26 -0600

    coin toss participation

---
+++ 1 - sum of three numbers (p)/Cointoss (p)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Cointoss__p_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Heads or Tails?");
+            string ans = Console.ReadLine().ToLower();
+
+            Random rand = new Random();
+            int toss = rand.Next(0, 2);
+
+            string coin;
+
+            if (toss == 0)
+            {
+                coin = "heads";
+            }
+            else
+            {
+                coin = "tails";
+            }
+            if (ans == coin)
+            {
+                Console.WriteLine($"You were right, it was {coin}!");
+            }
+            else
+            {
+                Console.WriteLine($"That's wrong, you suck, it was {coin}.");
+            }
+            
+        }
+    }
+}
