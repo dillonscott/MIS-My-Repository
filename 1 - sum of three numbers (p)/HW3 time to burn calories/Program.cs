2e37ad5d1bb6788d31549d765643cba6e075d846
Dillon Scott <dillonscott@ou.edu>
2021-03-09 19:51:55 -0600

    hw3 time to burn cals

---
+++ 1 - sum of three numbers (p)/HW3 time to burn calories/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW3_time_to_burn_calories
+{
+    class Program
+    {
+        const double calspermin = 3.9;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many calories do you want to burn this session?");
+            string ans = Console.ReadLine();
+
+            double cals;
+            int min = 1;
+            double calsburned = 0;
+
+            if (double.TryParse(ans, out cals) == false)
+            {
+                Console.WriteLine("INVALID INPUT. GOODBYE.");
+                Environment.Exit(-1);
+            }
+            else
+            {
+                while (calsburned < cals)
+                {
+                    min++;
+                    calsburned = calsburned + calspermin;
+                    Console.WriteLine($"After {min} minutes, you have burned {calsburned.ToString("N2")} calories.");
+                }
+            }
+        }
+    }
+}
