e152d32d0c9a80a309ec081cadbb80065925ceb3
Dillon Scott <dillonscott@ou.edu>
2021-03-09 21:22:54 -0600

    i337 converter

---
+++ 1 - sum of three numbers (p)/hw 4 i337 converter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace hw_4_i337_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a sentence >>");
+            string sentence = Console.ReadLine().ToUpper();
+
+            string newsentence = sentence.Replace("A", "4").Replace("E","3").Replace("H","|-|").Replace("S","$").Replace("T","7").Replace("U","|_|").Replace("O","0").Replace("P","[]D");
+            
+
+            if (newsentence[newsentence.Length-1] != '!')
+            {
+                newsentence = newsentence + '!';
+            }
+            Console.WriteLine(newsentence);
+        }
+    }
+}
