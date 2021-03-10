c6bbfb361924535ca9a57841829110d4c09d79a0
Dillon Scott <dillonscott@ou.edu>
2021-03-09 19:08:22 -0600

    hw1 salestotal

---
+++ 1 - sum of three numbers (p)/HW1 salestotal/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HW1_salestotal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of the product you are purchasing?");
+            string product = Console.ReadLine();
+
+            Console.WriteLine($"How many {product}'s do you want to buy?");
+            double quantity = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"What is the price of one {product}");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            double subtot = quantity * price;
+            double tax = subtot * .05;
+            double total = subtot + tax;
+
+            Console.WriteLine($"\tYour subtotal for your bill is {subtot}.");
+            Console.WriteLine($"\tYour sales tax for your bill is {tax}.");
+            Console.WriteLine($"\tYour total for your bill is {total}.");
+
+        }
+    }
+}
