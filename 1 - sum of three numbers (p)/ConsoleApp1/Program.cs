08e676865c700aaab0abe73bd0ea3c49b22870f1
Dillon Scott <dillonscott@ou.edu>
2021-03-09 20:28:48 -0600

    hw3 tuition increase

---
+++ 1 - sum of three numbers (p)/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tuition = 12600;
+            for (int i = 1; i <= 7; i++)
+            {
+                tuition = tuition * 1.05;
+                Console.WriteLine($"The tuition after year {i} will be {tuition.ToString("C2")}.");
+            }
+
+            
+        }
+    }
+}
