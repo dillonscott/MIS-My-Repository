3ed6c9caa8943e4c48ee88cf096db69581ea4df0
Dillon Scott <dillonscott@ou.edu>
2021-03-09 19:40:28 -0600

    guess a numbver participation

---
+++ 1 - sum of three numbers (p)/guess a number participation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace guess_a_number_participation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a lower bound >>");
+            int lower = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter an upper bound >>");
+            int upper = Convert.ToInt32(Console.ReadLine());
+
+            Random rand = new Random();
+            int num = rand.Next(lower, upper);
+
+            Console.WriteLine($"Guess a number bewteen {lower} and {upper}");
+            string ans = Console.ReadLine();
+
+            int guess;
+
+            if (int.TryParse(ans, out guess) == false)
+            {
+                Console.WriteLine("INVALID GUESS, GOODBYE!");
+                Environment.Exit(-1);
+            }
+            else
+            {
+                while (guess != num)
+                {
+                    Console.WriteLine($"Sorry that's wrong, please guess again!");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("You got it right!");
+            }
+            
+            
+
+            
+
+
+        }
+    }
+}
