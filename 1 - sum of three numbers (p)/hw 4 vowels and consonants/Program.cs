13c45753e7c981f5ee23fad48309b4ba38a6c9ce
Dillon Scott <dillonscott@ou.edu>
2021-03-09 21:10:43 -0600

    hw 4 vowels and consonants

---
+++ 1 - sum of three numbers (p)/hw 4 vowels and consonants/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace hw_4_vowels_and_consonants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a sentence >>");
+            string sentence = Console.ReadLine().ToLower();
+
+            int vowels = 0;
+            int consonants = 0;
+
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                char letter = sentence[i];
+                if (letter == 'a' || letter =='e' || letter=='i' || letter=='o' || letter=='u')
+                {
+                    vowels++;
+                }
+                else
+                {
+                    consonants++;
+                }
+
+                
+
+            }
+
+            Console.WriteLine($"There were {vowels} vowels in your sentence.");
+            Console.WriteLine($"There were {consonants} consonants in your sentence.");
+        }
+    }
+}
