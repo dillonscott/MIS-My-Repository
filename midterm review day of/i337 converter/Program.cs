9a8ee913a1894f0f1d37a926510ba5f4694a78f9
Dillon Scott <dillonscott@ou.edu>
2021-03-10 10:33:52 -0600

    1337 conv3rter again

---
+++ midterm review day of/i337 converter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace i337_converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a sentence >>");
+            string sentence = Console.ReadLine().ToUpper();
+
+            string newsentence;
+
+            newsentence = sentence.Replace('A', '4').Replace('E', '3').Replace("H", "|-|")
+                .Replace('S', '$').Replace('T', '7').Replace("U", "|_|").Replace('O', '0').Replace("P", "[]D");
+
+            if (newsentence[newsentence.Length-1] != '!')
+            {
+                newsentence = newsentence + '!';
+            }
+            Console.WriteLine(newsentence);
+        }
+            
+
+    }
+}
