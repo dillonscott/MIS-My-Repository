215030923d6e1c5c38c015fecb273c236a5bd83e
Dillon Scott <dillonscott@ou.edu>
2021-03-22 13:09:26 -0500

    Collections: mode

---
+++ Collections- finding mode/Collections- finding mode/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections__finding_mode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+
+            List<double> examScores = new List<double>();
+            Dictionary<double, int> exams = new Dictionary<double, int>();
+
+            do
+            {
+                Console.WriteLine("Enter your exam score >>");
+                answer = Console.ReadLine();
+
+                double score = Convert.ToDouble(answer); // assumeing user knows what theyre doing
+                examScores.Add(score); //store value in collection to access later
+
+                Console.WriteLine("Do you have another exam score to ender, yes or no? ");
+                answer = Console.ReadLine();
+
+
+            } while (answer.ToLower()[0] == 'y');
+
+            double min = examScores[0];
+
+            foreach (double score in examScores)
+            {
+                if (score < min)
+                {
+                    min = score;
+                }
+
+                if (exams.ContainsKey(score) == true)
+                {
+                    exams[score] = exams[score] + 1;
+                }
+                else
+                {
+                    exams.Add(score, 1);
+                }
+
+            }
+
+            Console.WriteLine($"Your min score was {min.ToString("n2")}");
+            int mode = 0;
+            foreach (double scoreKey in exams.Keys)
+            {
+                if (exams[scoreKey] > mode)
+                {
+                    mode = exams[scoreKey];
+                }
+            }
+
+            foreach (double scoreKey in exams.Keys)
+            {
+                if (exams[scoreKey] == mode)
+                {
+                    Console.WriteLine($"{exams[scoreKey]} occurs the most");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many coins do you have total?");
+            string ans = Console.ReadLine();
+
+            int coins;
+
+            if (int.TryParse(ans, out coins) == false)
+            {
+                while (int.TryParse(ans, out coins) == false)
+                {
+                    Console.WriteLine("That was not a valid amount of coins, please try again >>");
+                    ans = Console.ReadLine();
+                }
+            }
+
+            string cointype;
+            double coinvalue;
+            double total = 0;
+
+
+            for (int i = 1; i <= coins; i++)
+            {
+                Console.WriteLine($"What type of coin is coin number {i}?");
+                cointype = Console.ReadLine().ToLower();
+
+                if (cointype == "penny")
+                {
+                    coinvalue = .01;
+                }
+                else if (cointype == "nickel")
+                {
+                    coinvalue = .05;
+                }
+                else if (cointype == "dime")
+                {
+                    coinvalue = .10;
+                }
+                else
+                {
+                    coinvalue = .25;
+                }
+
+                total = total + (coinvalue);
+            }
+
+
+            Console.WriteLine($"\nYou have {total.ToString("C4")} total.");
+
+        }
+    }
+}
+        
