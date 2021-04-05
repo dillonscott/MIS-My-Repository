110b54cb5e9d015e7a3d348d927de04d9a0e42f8
Dillon Scott <dillonscott@ou.edu>
2021-04-05 13:10:01 -0500

    Methods example

---
+++ Methods (functions) InClassExample/Methods (functions) InClassExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Methods__functions__InClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            do
+            {
+                Console.WriteLine("Please input your grade in the course >>");
+                double grade = Convert.ToDouble(Console.ReadLine());
+
+                string lg = ConvertGradeToLetter(grade);
+
+                Console.WriteLine($"Congrats you have a {lg} in the course!");
+
+                Console.WriteLine("Do you want to convert another grade?");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower()[0] == 'y');
+
+
+        }
+
+        /// <summary>
+        /// COnverts a standard grade (i.e. 92.5) to the appropriate letter grade (i.e. 'A')
+        /// </summary>
+        /// <param name="anyVariableWeWantThisBelongsToThisMEthod"> The grade to convert to a letter</param>
+        /// <returns></returns> Returns a string that is the letter grade if the input... N/A if below 0
+
+        static string ConvertGradeToLetter(double anyVariableWeWantThisBelongsToThisMEthod) 
+        {
+            string lettergrade;
+            if (anyVariableWeWantThisBelongsToThisMEthod >= 90)
+            {
+                lettergrade = "A";
+            }
+            else if (anyVariableWeWantThisBelongsToThisMEthod >= 80)
+            {
+                lettergrade = "B";
+            }
+            else if (anyVariableWeWantThisBelongsToThisMEthod >= 70)
+            {
+                lettergrade = "C";
+            }
+            else if (anyVariableWeWantThisBelongsToThisMEthod >= 60)
+            {
+                lettergrade = "D";
+            }
+            else if (anyVariableWeWantThisBelongsToThisMEthod >= 0)
+            {
+                lettergrade = "F";
+            }
+            else
+            {
+                lettergrade = "N/A";
+            }
+
+            return lettergrade;
+        }
+
+    }
+}
