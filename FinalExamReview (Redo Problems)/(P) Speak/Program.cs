629034b5342987ed2e6b8f51235191b840b6f328
Dillon Scott <dillonscott@ou.edu>
2021-05-04 15:47:56 -0500

    Start exam review

---
+++ FinalExamReview (Redo Problems)/(P) Speak/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _P__Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a type of animal");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine($"A {animal} makes the sound {Speak(animal)}");
+
+
+
+        }
+
+        static string Speak (string animal) 
+        {
+            
+
+            if (animal == "dog")
+            {
+               string sound = "Woof";
+            }
+            else if (animal == "cow")
+            {
+                sound = "MOOOOOOO";
+            }
+            else if (animal == "horse")
+            {
+                sound = "Neighhhhh";
+            }
+
+
+            return sound;
+        }
+
+      
+    }
+}
