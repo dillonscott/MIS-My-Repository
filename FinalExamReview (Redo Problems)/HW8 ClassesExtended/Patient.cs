3a3208dc0260f131c370a6bd52547d58e5c678bb
Dillon Scott <dillonscott@ou.edu>
2021-05-11 17:39:30 -0500

    finish hw8

---
+++ FinalExamReview (Redo Problems)/HW8 ClassesExtended/Patient.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HW8_ClassesExtended
+{
+    public class Patient
+    {
+        public string FirstName { get; set; }
+        public char MidInitial { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public bool FluVaccineYN { get; set; }
+        public Dictionary<DateTime, double> Weights { get; set; }
+        public List<string> Perscriptions { get; set; }
+
+        public Patient()
+        {
+            FirstName = "";
+            MidInitial = '\0';
+            LastName = "";
+            ID = -10;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            Perscriptions = new List<string>();
+
+        }
+        public Patient(int patID)
+        {
+            FirstName = "";
+            MidInitial = '\0';
+            LastName = "";
+            ID = patID;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            Perscriptions = new List<string>();
+        }
+        public Patient(string firstName, char midin, string lastName)
+        {
+            FirstName = firstName;
+            MidInitial = midin;
+            LastName = lastName;
+            ID = -10;
+            FluVaccineYN = false;
+            Weights = new Dictionary<DateTime, double>();
+            Perscriptions = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            string vaxYN;
+            if (FluVaccineYN)
+            {
+                vaxYN = "True";
+            }
+            else
+            {
+                vaxYN = "False";
+            }
+            return $"{LastName}, {FirstName} {MidInitial}. ({ID}) Flu Vaccine: {vaxYN}";
+        }
+
+        public double CalculateMaxWeight() 
+        {
+            double max = -10;
+            foreach (double weight in Weights.Values)
+            {
+                if (weight > max)
+                {
+                    max = weight;
+                }
+
+            }
+            return max;
+        }
+
+        public void DisplayMedicine() 
+        {
+            foreach (string med in Perscriptions)
+            {
+                Console.WriteLine(med);
+            }
+        
+        }
+
+        public void AdministerVaccine() 
+        {
+            if (FluVaccineYN)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The patient has already been administered the vaccine!");
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Give this bitch the vaccine!");
+                Console.ResetColor();
+                FluVaccineYN = true;
+            }
+        }
+
+        public void PerscribeMedicine(string med) 
+        {
+            Perscriptions.Add(med);
+        }
+    }
+}
---
+++ FinalExamReview (Redo Problems)/HW8 ClassesExtended/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HW8_ClassesExtended
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] patientsInfo = File.ReadAllLines("AppointmentInformation.csv");
+            List<Patient> patients = new List<Patient>();
+
+            for (int i = 1; i < patientsInfo.Length; i++)
+            {
+                string patientInfo = patientsInfo[i];
+                string[] pieces = patientInfo.Split(',');
+                //AppointmentDate,FirstName,MiddleInitial,LastName,ID,FluVaccine,Weight,AddMedications
+                //      0             1          2           3      4      5       6         7
+
+                Patient p = new Patient();
+                p.FirstName = pieces[1];
+                p.MidInitial = Convert.ToChar(pieces[2]);
+                p.LastName = pieces[3];
+                p.ID = Convert.ToInt32(pieces[4]);
+                if (pieces[5] == "Y")
+                {
+                    p.FluVaccineYN = true;
+                }
+                else
+                {
+                    p.FluVaccineYN = false;
+                }
+                p.Weights.Add(Convert.ToDateTime(pieces[0]), Convert.ToDouble(pieces[6]));
+                p.Perscriptions.Add(pieces[7]);
+
+                patients.Add(p);
+                
+            }
+
+            foreach (Patient pat in patients)
+            {
+                Console.WriteLine($"\n{ pat}");
+                pat.PerscribeMedicine("That good good (Tramadol)");
+                pat.DisplayMedicine();
+                double maxWeight = pat.CalculateMaxWeight();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{pat.FirstName} {pat.LastName} had a max weight of {maxWeight} pounds");
+                Console.ResetColor();
+                pat.AdministerVaccine();
+
+                
+
+                    
+
+                
+            }
+
+            
+
+            
+
+
+
+
+
+        }
+    }
+}
